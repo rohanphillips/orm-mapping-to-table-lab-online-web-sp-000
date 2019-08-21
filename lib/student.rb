9848e656b3167fa7486746e5c9abd1dcf0b574b1
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-21 14:26:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,7 @@ class Student
       CREATE TABLE IF NOT EXISTS students(
         id INTEGER PRIMARY KEY,
         name TEXT,
-        grade FLOAT
+        grade TEXT
       );
       SQL
     DB[:conn].execute(sql)
